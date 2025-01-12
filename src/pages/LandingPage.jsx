5b401cd586c87afe22e9653db1b3ae6def24ad12
Bryan Lomerio <bryanlomerioaninogithub@gmail.com>
2025-01-12 17:29:50 +0800

    chore: change title

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -39,7 +39,7 @@ const LandingPage = () => {
 
     return (
         <div className="container mx-auto p-8">
-            <h1 className="text-3xl font-bold mb-6 text-center">DS Play</h1>
+            <h1 className="text-3xl font-bold mb-6 text-center">DS Visualization</h1>
 
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                 <Card
