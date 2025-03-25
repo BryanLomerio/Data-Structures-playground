71605397496fddba7af9c8f5b0cf3a1f950ab187
AninoDev <bryanlomerioaninogithub@gmail.com>
2025-03-25 22:38:28 +0800

    fix: vercel error

--- src/components/array/ArrayVisualizer.jsx
+++ src/components/array/ArrayVisualizer.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-import ArrayControls from "./arrayControls"
+import ArrayControls from "./ArrayControls"
 import { ArrowLeft } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
